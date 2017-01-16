2071f6f516cb364375b0758d3fb1e56a663e5523
Mark Servilla <mark.servilla@gmail.com>
2017-01-15 22:35:37 -0700

    Add db directory

---
+++ tests/test_pasta2glacier.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: test_pasta2glacier
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    1/15/17
+"""
+
+import unittest
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S%z')
+logging.getLogger('').setLevel(logging.WARN)
+logger = logging.getLogger('test_pasta2glacier')
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
