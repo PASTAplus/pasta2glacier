e17e61286f09f24066b5e98da70fe950a1fde3ad
Mark Servilla <mark.servilla@gmail.com>
2017-01-15 22:37:03 -0700

    Add base code for directory listings.

--- src/pasta2glacier.py
+++ src/pasta2glacier.py
@@ -4,6 +4,7 @@
 """:Mod: pasta2glacier
 
 :Synopsis:
+    To add PASTA data packages to the Amazon AWS Glacier storage.
 
 :Author:
     servilla
@@ -11,13 +12,21 @@
 :Created:
     1/15/17
 """
-
+import os
 import logging
 
 logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
                     datefmt='%Y-%m-%d% H:%M:%S%z')
 logging.getLogger('').setLevel(logging.WARN)
 logger = logging.getLogger('pasta2glacier')
+
+
+def getDataDirectories(path=None):
+
+    if path and os.path.isdir(path):
+        directories = os.listdir(path)
+
+    return directories
 
 
 def main():
