9a184b35616c8c476c14c5428975bdc2fde4a5a9
Mark Servilla <mark.servilla@gmail.com>
2017-01-16 21:25:13 -0700

    Add main iterative loop into main function

--- src/pasta2glacier.py
+++ src/pasta2glacier.py
@@ -13,6 +13,7 @@
     1/15/17
 """
 import os
+import shutil
 import logging
 
 logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
@@ -20,16 +21,32 @@ logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
 logging.getLogger('').setLevel(logging.WARN)
 logger = logging.getLogger('pasta2glacier')
 
+from glacier_db import GlacierDb
+
 
 def getDataDirectories(path=None):
-
     if path and os.path.isdir(path):
         directories = os.listdir(path)
-
     return directories
 
 
 def main():
+
+    data_path = '../data'
+    archive_path = '../archive'
+
+    gdb = GlacierDb()
+    gdb.connect_glacier_upload_log_db()
+
+    for dir_name in getDataDirectories(data_path):
+        if not gdb.package_exists(dir_name):
+            shutil.make_archive(base_name=archive_path + '/' + dir_name,
+                                format='gztar', base_dir=dir_name,
+                                root_dir=data_path)
+            # TODO: perform Glacier multi-part upload here
+            # TODO: register upload in glacier_db
+            os.remove(archive_path + '/' + dir_name + '.tar.gz')
+
     return 0
 
 
