bb168c379779efd57ecd860f3adfc2e5612a1617
servilla <mark.servilla@gmail.com>
2017-01-20 21:57:00 -0700

    Add single part archive upload method

--- src/glacier.py
+++ src/glacier.py
@@ -13,10 +13,6 @@
 """
 
 import logging
-
-logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
-                    datefmt='%Y-%m-%d% H:%M:%S%z')
-logging.getLogger('').setLevel(logging.WARN)
 logger = logging.getLogger('glacier')
 
 import boto3
@@ -30,8 +26,8 @@ class Glacier(object):
         self.client = boto3.client('glacier')
 
 
-    def do_multipart_upload(self, archive=None,
-                            archive_description='', part_size='4194304'):
+    def do_multipart_upload(self, archive=None, archive_description='',
+                            part_size='4194304'):
 
         response = self.client.initiate_multipart_upload(
             vaultName=self.vault_name,
@@ -42,11 +38,8 @@ class Glacier(object):
 
         block_size = (1024 ** 2) * 4  # 4MB
         range_start = 0
-        bytes = 0
 
         try:
-            print('------{archive}------'.format(
-                archive=archive))
             with open(archive, 'rb') as f:
                 block = f.read(block_size)
                 while block:
@@ -60,18 +53,17 @@ class Glacier(object):
                         range=range,
                         body=block)
 
-                    print('{bytes} -- {range}'.format(bytes=bytes, range=range))
                     range_start = range_start + bytes
                     block = f.read(block_size)
 
                 f.close()
                 archive_size = str(range_start)
-                upload_resposne = self.client.complete_multipart_upload(
-                    vaultName=self.vault_name,
-                    uploadId=upload_id,
-                    archiveSize=archive_size,
-                    checksum=utils.calculate_tree_hash(open(archive, 'rb'))
-                )
+            upload_response = self.client.complete_multipart_upload(
+                vaultName=self.vault_name,
+                uploadId=upload_id,
+                archiveSize=archive_size,
+                checksum=utils.calculate_tree_hash(open(archive, 'rb')))
+            return upload_response
         except:
             self.client.abort_multipart_upload(vaultName=self.vault_name,
                                                uploadId=upload_id)
@@ -79,13 +71,36 @@ class Glacier(object):
                 archive=archive)
             raise MultipartUploadError(err_msg)
 
-        return upload_resposne
+
+    def do_upload(self, archive=None, archive_description=''):
+
+        try:
+            f = open(archive, 'rb')
+            block = f.read()
+            upload_response = self.client.upload_archive(
+                vaultName=self.vault_name,
+                archiveDescription=archive_description, body=block)
+            f.close()
+            return upload_response
+        except:
+            err_msg = 'Failed to upload archive: {archive}'.format(
+                archive=archive)
+            raise UploadError(err_msg)
 
 
 class MultipartUploadError(Exception):
 
     def __init__(self,*args,**kwargs):
         Exception.__init__(self,*args,**kwargs)
+
+
+
+class UploadError(Exception):
+
+    def __init__(self,*args,**kwargs):
+        Exception.__init__(self,*args,**kwargs)
+
+
 
 
 def main():
