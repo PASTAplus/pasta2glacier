72de5aa8a081587aa8aeb55c9529d5a59044b3dc
servilla <mark.servilla@gmail.com>
2017-01-20 21:58:30 -0700

    Add logic for single and multi-part uploads

--- src/pasta2glacier.py
+++ src/pasta2glacier.py
@@ -14,11 +14,12 @@
 """
 import os
 import shutil
+from datetime import datetime
 import logging
 
 logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
-                    datefmt='%Y-%m-%d% H:%M:%S%z')
-logging.getLogger('').setLevel(logging.WARN)
+                    datefmt='%Y-%m-%d% H:%M:%S%z', filename='../p2g.log',
+                    level=logging.INFO)
 logger = logging.getLogger('pasta2glacier')
 
 from glacier_db import GlacierDb
@@ -26,26 +27,32 @@ from glacier import Glacier
 
 
 def getDataDirectories(path=None):
-    if path and os.path.isdir(path):
-        directories = os.listdir(path)
-    return directories
 
+    if path and os.path.isdir(path):
+        return os.listdir(path)
+    else:
+        err_msg = 'Path "{path}" is not a directory.'.format(path=path)
+        raise IOError(err_msg)
 
 def main():
 
     data_path = '../data'
     archive_path = '../archive'
+    multipart_threshold = (1024 ** 2) * 4  # 4MB
 
-    gdb = GlacierDb()
+    gdb = GlacierDb('glacier_upload_log.sqlite')
     gdb.connect_glacier_upload_log_db()
 
 
     for dir_name in getDataDirectories(data_path):
+        logger.info('Directory: {dir_name}'.format(dir_name=dir_name))
         if not gdb.package_exists(dir_name):
             archive = shutil.make_archive(base_name=archive_path + '/' +
                                                     dir_name,
                                           format='gztar', base_dir=dir_name,
                                           root_dir=data_path)
+            logger.info('Create archive: {archive}'.format(archive=archive))
+            archive_size = os.path.getsize(archive)
 
             archive_description = 'This archive ({dir_name}) is a data ' \
                                   'package derived from the PASTA ecological ' \
@@ -54,11 +61,24 @@ def main():
                                   'data package are licensed under Creative ' \
                                   'Commons CC-0.'.format(dir_name=dir_name)
 
-            g = Glacier(vault_name='PASTA_Test')
-            response = g.do_multipart_upload(archive=archive,
+            glacier = Glacier(vault_name='PASTA_Test')
+
+            if (archive_size < multipart_threshold):
+                response = glacier.do_upload(archive=archive,
                                         archive_description=archive_description)
-            print(response)
-            # TODO: register upload in glacier_db
+            else:
+                response = glacier.do_multipart_upload(archive=archive,
+                                        archive_description=archive_description)
+
+            logger.info('Response: {response}'.format(response=response))
+
+            gdb.add_upload_record(package=dir_name,
+                                  identifier=response['archiveId'],
+                                  location=response['location'],
+                                  size=archive_size,
+                                  checksum=response['checksum'],
+                                  timestamp=datetime.now())
+
             os.remove(archive)
 
     return 0
