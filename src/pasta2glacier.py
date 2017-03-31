efcadb81cfe676697b16aa717e958c51955206c4
servilla <mark.servilla@gmail.com>
2017-03-30 20:53:31 -0600

    Add docopts command line argument parser

--- src/pasta2glacier.py
+++ src/pasta2glacier.py
@@ -13,15 +13,19 @@
 :Created:
     1/15/17
 """
+
 import os
 import shutil
 from datetime import datetime
 import logging
+import sys
+
+from boto3.exceptions import Boto3Error
+from docopt import docopt
 
 from glacier_db import GlacierDb
 from glacier import Glacier
 from glacier import GlacierUploadError
-
 
 logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S%z', filename='p2g.log',
@@ -30,7 +34,7 @@ logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
 logger = logging.getLogger('pasta2glacier')
 
 
-def getDataDirectories(path=None):
+def data_directories(path=None):
 
     if path and os.path.isdir(path):
         return os.listdir(path)
@@ -38,9 +42,36 @@ def getDataDirectories(path=None):
         err_msg = 'Path "{path}" is not a directory.'.format(path=path)
         raise IOError(err_msg)
 
-def main():
 
-    DRY_RUN = True
+def mock_response():
+    r = {}
+    r['archiveId'] = '2bf5l58c52IXU67qovKRk49Np5vYACZELD4rhWOL86kIi3-ncU-WM-v7hG8l9TuTiT12xmiloa67WsMUGdrxOKVejKdFYA5F7Ksmdlifq_gARt6a7rwR5oFDl5jyH3ACjGh8P83eBg'
+    r['location'] = '/321492031925/vaults/PASTA_Test/archives/2bf5l58c52IXU67qovKRk49Np5vYACZELD4rhWOL86kIi3-ncU-WM-v7hG8l9TuTiT12xmiloa67WsMUGdrxOKVejKdFYA5F7Ksmdlifq_gARt6a7rwR5oFDl5jyH3ACjGh8P83eBg'
+    r['checksum'] = '226cb0468dcda728f80bd5807e6483cbbb191b5f9e7cb0b61fd51c11acb30d01'
+    return r
+
+
+def main(argv):
+    """
+    pasta2glacier provides a mechanism to upload archived (zip or tar) data
+    packages from the PASTA data repository into Amazon's AWS Glacier storage.
+
+    Usage:
+        pasta2glacier.py <vault> [-d | --dry] 
+        pasta2glacier.py (-h | --help)
+        
+    Arguments:
+        vault       The AWS Glacier vault to be used (e.g. "PASTA_Test")
+
+    Options:
+      -h --help     This page
+      -d --dry      Dry run only - no AWS Glacier upload
+        
+    """
+
+    args = docopt(str(main.__doc__))
+    DRY_RUN = args['--dry']
+    vault = args['<vault>']
 
     data_path = '../data'
     archive_path = '../archive'
@@ -49,8 +80,16 @@ def main():
     gdb = GlacierDb('glacier_upload_log.sqlite')
     gdb.connect_glacier_upload_log_db()
 
+    glacier = None
 
-    for dir_name in getDataDirectories(data_path):
+    if not DRY_RUN:
+        try:
+            glacier = Glacier(vault_name=vault)
+        except Boto3Error as e:
+            logger.error(e)
+            return -1
+
+    for dir_name in data_directories(data_path):
         logger.info('Directory: {dir_name}'.format(dir_name=dir_name))
         if not gdb.package_exists(dir_name):
             archive = shutil.make_archive(base_name=archive_path + '/' +
@@ -62,10 +101,9 @@ def main():
 
             archive_description = '{dir_name}'.format(dir_name=dir_name)
 
-            if not DRY_RUN:
-
-                glacier = Glacier(vault_name='PASTA_Test')
-
+            if DRY_RUN:
+                response = mock_response()
+            else:
                 try:
 
                     if (archive_size < multipart_threshold):
@@ -75,21 +113,22 @@ def main():
                         response = glacier.do_multipart_upload(archive=archive,
                                                 archive_description=archive_description)
 
-                    logger.info('Response: {response}'.format(response=response))
-
-                    gdb.add_upload_record(package=dir_name,
-                                          identifier=response['archiveId'],
-                                          location=response['location'],
-                                          size=archive_size,
-                                          checksum=response['checksum'],
-                                          timestamp=datetime.now())
                 except GlacierUploadError as e:
                     logger.error(e)
+                    return -1
 
+            logger.info('Response: {response}'.format(response=response))
+
+            gdb.add_upload_record(package=dir_name,
+                                  identifier=response['archiveId'],
+                                  location=response['location'],
+                                  size=archive_size,
+                                  checksum=response['checksum'],
+                                  timestamp=datetime.now())
             os.remove(archive)
 
     return 0
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
