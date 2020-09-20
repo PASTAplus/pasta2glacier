d628e2dd4e7c72857ed7d8b8d1501176f76727a1
Mark Servilla <mark.servilla@gmail.com>
2020-09-20 08:08:46 -0600

    Refactor to use pytest

--- tests/test_glacier_db.py
+++ tests/test_glacier_db.py
@@ -12,96 +12,96 @@
     1/15/17
 """
 from datetime import datetime
-import unittest
+from pathlib import Path
 
 import daiquiri
+import pytest
 from sqlalchemy.exc import IntegrityError
 
 from glacier_db import GlacierDb
 
 logger = daiquiri.getLogger('test_glacier_db.py: ' + __name__)
+DB = 'glacier_upload_log_test.sqlite'
 
 
-class TestDatabase(unittest.TestCase):
+@pytest.fixture()
+def gdb():
+    return GlacierDb(DB)
 
-    def setUp(self):
-        db_name = 'glacier_upload_log_test.sqlite'
-        self.gdb = GlacierDb(db_name)
-        self.gdb.connect_glacier_upload_log_db()
 
-    def tearDown(self):
-        self.gdb.delete_glacier_upload_log_db()
-        pass
+@pytest.fixture()
+def cleanup():
+    yield
+    Path(DB).unlink(missing_ok=True)
 
-    def test_add_records(self):
-        self.gdb.add_upload_record(
+
+def test_add_records(gdb, cleanup):
+    gdb.add_upload_record(
+        package='knb-lter-nin.1.1',
+        identifier='2034saf0923',
+        location='https://aws/glacier/2034saf0923',
+        size=734563,
+        checksum='AB9F90234E234D',
+        timestamp=datetime.now())
+
+    gdb.add_upload_record(
+        package='knb-lter-nin.2.1',
+        identifier='823lkas09234',
+        location='https://aws/glacier/823lkas09234',
+        size=89234,
+        checksum='3243A12FD23',
+        timestamp=datetime.now())
+
+
+def test_record_collision(gdb, cleanup):
+    gdb.add_upload_record(
+        package='knb-lter-nin.1.1',
+        identifier='2034saf0923',
+        location='https://aws/glacier/2034saf0923',
+        size=734563,
+        checksum='AB9F90234E234D',
+        timestamp=datetime.now())
+
+    with pytest.raises(IntegrityError):
+        gdb.add_upload_record(
             package='knb-lter-nin.1.1',
             identifier='2034saf0923',
             location='https://aws/glacier/2034saf0923',
-            size=734563,
+            size='734563',
             checksum='AB9F90234E234D',
-            timestamp=datetime.now())
-
-        self.gdb.add_upload_record(
-            package='knb-lter-nin.2.1',
-            identifier='823lkas09234',
-            location='https://aws/glacier/823lkas09234',
-            size=89234,
-            checksum='3243A12FD23',
-            timestamp=datetime.now())
-
-    def test_record_collision(self):
-        self.gdb.add_upload_record(
-            package='knb-lter-nin.1.1',
-            identifier='2034saf0923',
-            location='https://aws/glacier/2034saf0923',
-            size=734563,
-            checksum='AB9F90234E234D',
-            timestamp=datetime.now())
-
-        self.assertRaises(IntegrityError,
-                          self.gdb.add_upload_record,
-                          package='knb-lter-nin.1.1',
-                          identifier='2034saf0923',
-                          location='https://aws/glacier/2034saf0923',
-                          size='734563',
-                          checksum='AB9F90234E234D',
-                          timestamp=datetime.now())
+            timestamp=datetime.now()
+        )
 
 
-    def test_package_exists(self):
-        self.gdb.add_upload_record(
-            package='knb-lter-nin.1.1',
-            identifier='2034saf0923',
-            location='https://aws/glacier/2034saf0923',
-            size=734563,
-            checksum='AB9F90234E234D',
-            timestamp=datetime.now())
+def test_package_exists(gdb, cleanup):
+    gdb.add_upload_record(
+        package='knb-lter-nin.1.1',
+        identifier='2034saf0923',
+        location='https://aws/glacier/2034saf0923',
+        size=734563,
+        checksum='AB9F90234E234D',
+        timestamp=datetime.now())
 
-        self.gdb.add_upload_record(
-            package='knb-lter-nin.2.1',
-            identifier='823lkas09234',
-            location='https://aws/glacier/823lkas09234',
-            size=89234,
-            checksum='3243A12FD23',
-            timestamp=datetime.now())
+    gdb.add_upload_record(
+        package='knb-lter-nin.2.1',
+        identifier='823lkas09234',
+        location='https://aws/glacier/823lkas09234',
+        size=89234,
+        checksum='3243A12FD23',
+        timestamp=datetime.now())
 
-        self.gdb.add_upload_record(
-            package='knb-lter-nin.3.1',
-            identifier='ljd92ls2klj',
-            location='https://aws/glacier/ljd92ls2klj',
-            size=92348,
-            checksum='345B03E223F',
-            timestamp=datetime.now())
+    gdb.add_upload_record(
+        package='knb-lter-nin.3.1',
+        identifier='ljd92ls2klj',
+        location='https://aws/glacier/ljd92ls2klj',
+        size=92348,
+        checksum='345B03E223F',
+        timestamp=datetime.now())
 
-        # Test for existing package
-        exists = self.gdb.package_exists(package='knb-lter-nin.3.1')
-        self.assertTrue(exists)
+    # Test for existing package
+    exists = gdb.package_exists(package='knb-lter-nin.3.1')
+    assert exists
 
-        # Test for non-existing package
-        exists = self.gdb.package_exists(package='knb-lter-non.3.1')
-        self.assertFalse(exists)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    # Test for non-existing package
+    exists = gdb.package_exists(package='knb-lter-non.3.1')
+    assert not exists
