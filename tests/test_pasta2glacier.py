9b12357d223b2e88a1cbaf8029d99ab62cf7ac6d
Mark Servilla <mark.servilla@gmail.com>
2020-09-20 08:12:35 -0600

    Refactor to use pytest

--- tests/test_pasta2glacier.py
+++ tests/test_pasta2glacier.py
@@ -11,28 +11,15 @@
 :Created:
     1/15/17
 """
-import unittest
-
 import daiquiri
+import pytest
 
 import pasta2glacier
 
 logger = daiquiri.getLogger('test_pasta2glacier.py: ' + __name__)
 
 
-class TestGetDataDirectories(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_directory_listing(self):
-        data_path = '../data'
-        dir_names = pasta2glacier.data_directories(path=data_path)
-        self.assertIsNotNone(dir_names)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_directory_listing():
+    data_path = '../data'
+    dir_names = pasta2glacier.data_directories(path=data_path)
+    assert dir_names is not None
